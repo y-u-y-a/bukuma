fdd82fd12f32cf762a13ca8095556271b4970f28
y-u-y-a <yuya.manage@gmail.com>
2019-09-05 22:39:16 +0900

    孫要素までの取得に成功

--- index.js
+++ index.js
@@ -6,72 +6,160 @@
 // ブックマークの取得
 chrome.bookmarks.getTree(function(bookmarks){
 
-  // バーのフォルダを取得
-  var barFolders = bookmarks[0].children[0].children;
-  // その他のブックマーク
-  var others      = bookmarks[0].children[1];
+
+  var parent = bookmarks[0];
+
+  // // 子要素の数だけ繰り返す
+  // for(var i in parent.children){
+  //   // parentがフォルダの場合(フォルダの表示と子要素を取得しparentを更新)
+  //   if(parent.children[i].url == undefined){
+  //     // フォルダを取得・表示 /////////////////////////////////
+  //     if(parent.children[i].url == undefined){
+  //       var parentName = "●" + parent.children[i].title;
+  //       ulTag(parentName);
+  //     }
+  //     // parent = parent.children[i];
+  //   }
+  //   // parentがURLの場合
+  //   if(parent.children[i].url != undefined){
+  //     // 中身の表示 /////////////////////////////////////////
+  //     createBookmarks(parent.children);
+  //   }
+  //   console.log(parent.children[i].url);
+  // }
 
 
-  // １つ目のフォルダーごとに繰り返し
-  for(var i in barFolders){
 
-    var folderName = barFolders[i].title;
-    // フォルダ名の表示
-    // showFolderName(folderName);
-    // console.log("●：" + folderName); // フォルダ名の取得
 
-    for(var j in barFolders[i].children){
 
-      var bookName = barFolders[i].children[j].title;
-      var url      = barFolders[i].children[j].url;
 
-      // フォルダの場合(undefinedだから)
-      if(url != undefined){
-        var domain   = url.match(/^https?:\/{2,}(.*?)(?:\/|\?|#|$)/)[1]; // エスケープ(\を直前に使用する)
+  // フォルダの取得(ブックマークバー) ===============================================
+  for(var i in parent.children){
+
+    var parentName = "●" + parent.children[i].title;
+
+    // ①フォルダの場合(開封)
+    if(parent.children[i].url == undefined){
+      ulTag(parentName);
+
+      // フォルダの取得(２つ目="公式、API"とか) =====================================
+      for(var j in parent.children[i].children){
+
+        var childName = parent.children[i].children[j].title;
+        var url       = parent.children[i].children[j].url;
+
+        // ②フォルダの場合(開封)
+        if(url == undefined){
+          var childName = "●" + childName;
+          ulTag(childName);
+
+          // フォルダの取得(３つ目="JavaScript"<フロントとか)========================
+          for(var k in parent.children[i].children[j].children){
+
+            var oldChildName = parent.children[i].children[j].children[k].title;
+            var url          = parent.children[i].children[j].children[k].url;
+
+            // ③フォルダの場合(開封)
+            if(url == undefined){
+              var oldChildName = "●" + oldChildName;
+              ulTag(oldChildName);
+            }
+            // ③ブックマークの場合
+            if(url != undefined){
+              createBookmarks(childName, oldChildName, url);
+            }
+          }
+        }
+        // ②ブックマークの場合
+        if(url != undefined){
+          createBookmarks(parentName, childName, url);
+        }
       }
-
-      var fabicon  = "https://www.google.com/s2/favicons?domain=" + domain;
-
-      // DOMの生成
-      liTag(bookName);
-      imgTag(bookName, fabicon);
-      aTag(bookName, url);
-
-      // console.log(bookName + "\n" + url); // 中身の取得
-      // console.log(fabicon);
+    }
+    // ①ブックマークの場合
+    if(parent.children[i].url != undefined){
     }
   }
-  // console.log(others.title);
 
+  var encode = JSON.stringify(parent.children[0].url, null, 4);
+  console.log(encode);
 
   //////////////////////////////////////////////////////////////////////////////
   // ここから関数
   //////////////////////////////////////////////////////////////////////////////
 
-  function showFolderName(folderName){
-    var div = document.getElementById("folder");
-    div.textContent = folderName;
+  // function getAllBookMarks(child){
+  //   for(var k in child.children){
+
+  //     var oldChildName = child.children[k].title;
+  //     var url          = child.children[k].url;
+
+  //     // ③フォルダの場合(開封)
+  //     if(url == undefined){
+  //       var oldChildName = "●" + oldChildName;
+  //       ulTag(oldChildName);
+  //     }
+  //     // ③ブックマークの場合
+  //     if(url != undefined){
+  //       createBookmarks(childName, oldChildName, url);
+  //     }
+  //   }
+  // }
+
+  function createBookmarks(parentName, childName, url){
+    var domain  = url.match(/^https?:\/{2,}(.*?)(?:\/|\?|#|$)/);
+    var fabicon = "https://www.google.com/s2/favicons?domain=" + domain;
+    // DOMの生成
+    liTag(parentName, childName);
+    imgTag(childName, fabicon);
+    aTag(childName, url);
   }
 
-  function liTag(bookName){
+  // // 子要素urlの取得・表示(parentは配列のキー)
+  // function createBookmarks(parent){
+  //   for(var j in parent){
+  //     var bookName = parent[j].title;
+  //     var url       = parent[j].url;
+  //     // ブックマークの場合
+  //     if(url != undefined){
+  //     // var domain    = url.match(/^https?:\/{2,}(.*?)(?:\/|\?|#|$)/)[1];
+  //     // var fabicon   = "https://www.google.com/s2/favicons?domain=" + domain;
+  //     }
+  //     // フォルダの場合
+  //     else{
+  //     }
+  //     // DOMの生成
+  //     liTag(bookName);
+  //     // imgTag(bookName, fabicon);
+  //     aTag(bookName, url);
+  //   }
+  // }
+
+  function ulTag(parentName){
+    var ul = document.createElement("ul");
+    ul.setAttribute("id", parentName);
+    ul.textContent = parentName;
+    document.getElementById("folder").append(ul);
+  }
+
+  function liTag(parentName, childName){
     var li = document.createElement("li");
-    li.setAttribute("id", bookName);
-    document.getElementById("bookLists").appendChild(li);
+    li.setAttribute("id", childName);
+    document.getElementById(parentName).appendChild(li);
   }
 
   // fabiconの画像の表示
-  function imgTag(bookName, fabicon){
+  function imgTag(childName, fabicon){
     var img = document.createElement("img");
     img.src = fabicon;
-    document.getElementById(bookName).appendChild(img);
+    document.getElementById(childName).appendChild(img);
   }
 
-  function aTag(bookName, url){
+  function aTag(childName, url){
     var a  = document.createElement("a");
     a.href = url;
-    a.textContent = bookName;
-    document.getElementById(bookName).appendChild(a);
+    a.textContent = childName;
+    document.getElementById(childName).appendChild(a);
   }
-
 
 });
