910a945af72e25f28e5c151647ff7b9374ce44c7
y-u-y-a <yuya.manage@gmail.com>
2019-09-06 17:11:14 +0900

    アルゴリズムの完成

--- index.js
+++ index.js
@@ -1,96 +1,24 @@
-// // chromeアプリの取得
-// chrome.management.getAll(function(apps) {
-//   console.log(apps);
-// });
 
 // ブックマークの取得
 chrome.bookmarks.getTree(function(bookmarks){
 
+  var parent   = bookmarks[0];
+  parent.title = "folder";
+  createBranch(parent);
 
-  var parent = bookmarks[0];
-
-  // // 子要素の数だけ繰り返す
-  // for(var i in parent.children){
-  //   // parentがフォルダの場合(フォルダの表示と子要素を取得しparentを更新)
-  //   if(parent.children[i].url == undefined){
-  //     // フォルダを取得・表示 /////////////////////////////////
-  //     if(parent.children[i].url == undefined){
-  //       var parentName = "●" + parent.children[i].title;
-  //       ulTag(parentName);
-  //     }
-  //     // parent = parent.children[i];
-  //   }
-  //   // parentがURLの場合
-  //   if(parent.children[i].url != undefined){
-  //     // 中身の表示 /////////////////////////////////////////
-  //     createBookmarks(parent.children);
-  //   }
-  //   console.log(parent.children[i].url);
-  // }
-
-
-
-
-
-  var TreeName = "folder";
-  // フォルダの繰り返し(ブックマークバー, その他のブックマーク) ===============================================
-  for(var i in parent.children){
-    var parentName = "●" + parent.children[i].title;
-
-    // ①フォルダの作成(ul要素作成)
-    if(parent.children[i].url == undefined){
-      ulTag(TreeName, parentName);
-
-      // フォルダの繰り返し(２つ目="公式、API、就職.....") =====================================
-      // createBranch(parent.children[i]);
-      for(var j in parent.children[i].children){
-        var childName = "●" + parent.children[i].children[j].title;
-        var url       = parent.children[i].children[j].url;
-
-        // ②フォルダの作成(ul要素作成)
-        if(url == undefined){
-          ulTag(parentName, childName);
-
-          // フォルダの取得(３つ目="JavaScript"<フロント)========================
-          createBranch(parent.children[i].children[j]);
-          // for(var k in parent.children[i].children[j].children){
-          //   var oldChildName = "●" + parent.children[i].children[j].children[k].title;
-          //   var url          = parent.children[i].children[j].children[k].url;
-          //   // ③フォルダの場合(ul要素作成)
-          //   if(url == undefined){
-          //     ulTag(childName, oldChildName);
-          //   }
-          //   // ③ブックマークの作成
-          //   else{
-          //     createBookmarks(childName, oldChildName, url);
-          //   }
-          // }
-        }
-        // ②ブックマークの作成
-        else{
-          createBookmarks(parentName, childName, url);
-        }
-      }
-    }
-    // ①ブックマークの作成
-    else{
-    }
-  }
-
-  var encode = JSON.stringify(parent.children[0].url, null, 4);
-  console.log(encode);
+  // var encode = JSON.stringify(parent, null, 4);
+  // console.log(encode);
 
   //////////////////////////////////////////////////////////////////////////////
-  // ここから関数
+  // フォルダの取得(ul作成)〜中身の繰り返し
   //////////////////////////////////////////////////////////////////////////////
-
-  // フォルダ(配列)を使って子要素の表示
+  // フォルダ(配列)のキーを使って子要素の表示
   function createBranch(dirKey){
-    // フォルダの名前の取得
-    var parentName = "●" + dirKey.title;
+    // フォルダ名の取得
+    var parentName = dirKey.title;
     // フォルダの繰り返し
     for(var i in dirKey.children){
-      var childName = "●" + dirKey.children[i].title;
+      var childName = dirKey.children[i].title;
       var url       = dirKey.children[i].url;
       // フォルダの場合(ul要素の作成)
       if(url == undefined){
@@ -104,17 +32,28 @@ chrome.bookmarks.getTree(function(bookmarks){
       }
     }
   }
-
+  //////////////////////////////////////////////////////////////////////////////
   // ブックマークの作成
+  //////////////////////////////////////////////////////////////////////////////
   function createBookmarks(parentName, childName, url){
     var domain  = url.match(/^https?:\/{2,}(.*?)(?:\/|\?|#|$)/);
-    var fabicon = "https://www.google.com/s2/favicons?domain=" + domain;
+    // chromeのURLなどはdomainを取得できてない
+    if(domain != null || domain != undefined){
+      domain = domain[1];
+      var fabicon = "https://www.google.com/s2/favicons?domain=" + domain;
+    }
+    else{
+      var fabicon = "";
+    }
     // DOMの生成
     liTag(parentName, childName);
     imgTag(childName, fabicon);
     aTag(childName, url);
   }
 
+  //////////////////////////////////////////////////////////////////////////////
+  // 要素の作成
+  //////////////////////////////////////////////////////////////////////////////
   function ulTag(parentName, childName){
     var ul = document.createElement("ul");
     ul.setAttribute("id", childName);
